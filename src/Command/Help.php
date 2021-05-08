2d7e1f6d62a2c8b7374169a59b4f39bfa5952a47
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-05-08 16:20:18 -0300

    Pass App to Help command

--- src/Command/Help.php
+++ src/Command/Help.php
@@ -4,9 +4,39 @@ declare(strict_types=1);
 
 namespace Griffin\Cli\Command;
 
+use Minicli\App;
+
+/**
+ * Help Command
+ */
 class Help
 {
-    public function __invoke()
+    /**
+     * App
+     */
+    protected App $app;
+
+    /**
+     * Constructor
+     *
+     * @param App $app App
+     */
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Retrieve App
+     *
+     * @return App Expected Object
+     */
+    public function getApp(): App
+    {
+        return $this->app;
+    }
+
+    public function __invoke(): void
     {
     }
 }
--- tests/Command/HelpTest.php
+++ tests/Command/HelpTest.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace Griffin\Cli\Command;
 
+use Griffin\Cli\Command\Help;
+use Minicli\App;
 use PHPUnit\Framework\TestCase;
 
 class HelpTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $this->app = $this->createMock(App::class);
+
+        $this->command = new Help($this->app);
+    }
+
+    public function testConstructor(): void
+    {
+        $this->assertSame($this->app, $this->command->getApp());
+    }
 }
