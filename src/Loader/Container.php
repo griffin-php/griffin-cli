d9c784c906e8e29319eefa759ebfb69320fe5f83
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-05-10 20:43:53 -0300

    Include docblocks

--- src/Loader/Container.php
+++ src/Loader/Container.php
@@ -7,8 +7,18 @@ namespace Griffin\Cli\Loader;
 use Psr\Container\ContainerInterface;
 use Throwable;
 
+/**
+ * PSR-11 Container
+ */
 class Container implements ContainerInterface
 {
+    /**
+     * Create Objects by Class Name Identifier
+     *
+     * @param string $id Class Name
+     * @throws Exception Class Not Found or Error Initializing Object
+     * @return mixed Expected Object
+     */
     public function get(string $id): mixed
     {
         if (! $this->has($id)) {
@@ -28,6 +38,12 @@ class Container implements ContainerInterface
         }
     }
 
+    /**
+     * Check if Class Exists
+     *
+     * @param string $id Class Name
+     * @return bool Expected Value
+     */
     public function has(string $id): bool
     {
         return class_exists($id);
