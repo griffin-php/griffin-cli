62612927f76916bec156ff449560dbf445c499f3
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-05-10 20:28:28 -0300

    Test exception class

---
+++ tests/Loader/ExceptionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GriffinTest\Cli\Loader;
+
+use Exception as BaseException;
+use Griffin\Cli\Loader\Exception;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\NotFoundExceptionInterface;
+
+class ExceptionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->exception = new Exception();
+    }
+
+    public function testInstanceOf(): void
+    {
+        $this->assertInstanceOf(BaseException::class, $this->exception);
+        $this->assertInstanceOf(NotFoundExceptionInterface::class, $this->exception); // PSR
+    }
+
+    public function testCodes(): void
+    {
+        $this->assertSame(1, Exception::CLASS_UNKNOWN);
+    }
+}
